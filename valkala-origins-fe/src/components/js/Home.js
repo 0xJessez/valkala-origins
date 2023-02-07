411f832f999e2c10ae9a894fb56a1318a9a2fc42
0xJessez <jesse.zhang919@gmail.com>
2023-02-07 11:16:23 +1100

    home screen added

---
+++ valkala-origins-fe/src/components/js/Home.js
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import '../css/Home.scss'
+
+function Home() {
+
+  return (
+    <div className="Home">
+      <header className="top-bar">
+        <div className="left-head">
+          <h2>VALKALA ORIGINS</h2>
+        </div>
+        <div className="right-head">
+          <h4>Leaderboard</h4>
+          <h4>Sign up</h4>
+          <h4>Log in</h4>
+        </div>
+      </header>
+
+    </div>
+  )
+}
+
+export default Home
