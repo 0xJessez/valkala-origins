16a220259d6f1db6cae0b2ea9def90b50bad6c4a
0xJessez <jesse.zhang919@gmail.com>
2023-02-08 14:51:00 +1100

    town updates

--- valkala-origins-fe/src/components/js/Town.js
+++ valkala-origins-fe/src/components/js/Town.js
@@ -5,10 +5,18 @@ export default function Town() {
   return (
     <div className="Town">
       <div className="town-area">
-        <div className="poi">barracks</div>
-        <div className="poi">summoning</div>
-        <div className="poi">blacksmith</div>
-        <div className="poi">dungeons</div>
+        <a href="#" className="hotspot" id='barracks'>
+          <span className="location-text">BARRACKS</span>
+        </a>
+        <a href="#" className="hotspot">
+          <span className="location-text">SUMMONING GROUND</span>
+        </a>
+        <a href="#" className="hotspot">
+          <span className="location-text">DUNGEONS</span>
+        </a>
+        <a href="#" className="hotspot">
+          <span className="location-text">BLACKSMITH</span>
+        </a>
       </div>
     </div>
   )
